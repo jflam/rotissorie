13879e19f56873d381ecd9c19c4192d8b29e533f
John Lam <jflam@microsoft.com>
2016-03-18 17:52:47 -0700

    KNN explorations

---
+++ baseball_knn.R
@@ -0,0 +1,4 @@
+# Simplest possible scenario is
+# 1. Single-season performances only
+# 2. Restrict to players who play more than 120 games in season
+# 3. Only offensive stats. H, R, RBI, 2B, 3B, HR
