1f387b9d7c5aa3e97971521ad1fcc6f32ed58473
John Lam <jlam@iunknown.com>
2016-03-20 14:20:07 -0700

    generate comparison table between training and test data set

--- baseball_knn.R
+++ baseball_knn.R
@@ -31,11 +31,11 @@ rollup_batting <- batting %>%
         AVG = H / AB,
         S = H - (X2B + X3B + HR),
         TB = S + 2 * X2B + 3 * X3B + 4 * HR,
-        SLG = TB / AB)
+        SLG = TB / AB,
+        ID = sprintf("%s:%s", playerID, yearID))
 
 batting_training <- rollup_batting %>%
     filter(yearID > 1990 & yearID < 2014 & G > minimum_game_threshold)
-
 
 batting_testing <- rollup_batting %>%
     filter(yearID == 2014 & G > minimum_game_threshold)
@@ -51,11 +51,33 @@ names(c4) <- "playerID"
 batting_validation <- rollup_batting %>%
     filter(yearID == 2015) %>%
     inner_join(c4)
-    
+
 # Ensure that we have the intersection 
 batting_testing <- batting_testing %>%
     inner_join(c4, by = "playerID")
 
-player_ids <- batting_training$playerID
+# Remove all non-numeric data
+training <- batting_training[3:17]
+testing <- batting_testing[3:17]
+validation <- batting_validation[3:17]
+
+player_ids <- batting_training$ID
 library(class)
-results <- knn(batting_training, batting_testing, player_ids, k = 3)
+results <- knn(training, testing, player_ids, k = 3)
+
+# Print out a report so we can eyeball the results
+library(DT)
+
+m0 <- batting_testing[c('playerID', 'G', 'R', 'X2B', 'X3B', 'HR', 'RBI', 'SB')]
+m1 <- as.data.frame(results)
+names(m1) <- "ID"
+m2 <- batting_training %>%
+    inner_join(m1, by = "ID") %>%
+    select(ID, G, R, X2B, X3B, HR, RBI, SB)
+
+# Create comparison
+
+comparison <- m0 %>%
+    inner_join(m2, by = "ID")
+
+    
